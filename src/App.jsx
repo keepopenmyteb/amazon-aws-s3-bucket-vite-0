4f69f4420a755751316a6d176afbb64cc0e4e40f
user <oprenber@gmail.com>
2025-12-30 12:03:56 +0900

    feat: 제목 변경

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Amazon-Aws-S3-Bucket</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
